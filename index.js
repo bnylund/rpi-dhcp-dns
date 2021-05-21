ce2b0fef4167abfdb8d4e8d5be08b43743b82937
Benjamin Nylund <31191383+bnylund@users.noreply.github.com>
2021-05-21 14:18:36 -0500

    added error handling

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var dhcp = require('dhcp');
 
+let client = 1;
+
 var s = dhcp.createServer({
     range: [
         "10.0.0.5", "10.255.255.254"
@@ -10,13 +12,36 @@ var s = dhcp.createServer({
         "24:4B:FE:55:76:FD": "10.0.0.2",
     },
     netmask: '255.0.0.0',
-    router: [],
     dns: ['10.0.0.1'],
-    hostname: 'rpi',
+    hostname: function() { return 'rpi-dhcp-client' + client++ },
     broadcast: '10.255.255.255',
     server: '10.0.0.1'
 });
 
-s.listen(null, '10.0.0.1', () => {
+s.on("error", (err) => {
+    console.log("--- DHCP ERROR ---");
+    console.log(err);
+    console.log("--- END DHCP ERROR ---");
+});
+
+s.on("message", (msg) => {
+    console.log("--- NEW DHCP MESSAGE ---");
+    console.log(msg);
+    console.log("--- END DHCP MESSAGE ---");
+});
+
+s.on('bound', (state) => {
+    console.log(state);
+})
+
+s.on("listening", () => {
+    console.log("dhcp listening!");
+});
+
+s.on("close", () => {
+    console.log("dhcp server closed.");
+});
+
+s.listen(67, '0.0.0.0', () => {
     console.log("dhcp server started!");
 });
